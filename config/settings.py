737533090d2b78ff944d552b33aa0aa135c2dad4
Miguel <miguelangelgambero@gmail.com>
2026-05-18 10:10:54 +0200

    Configuración de nube para Neon y Cloudinary

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,13 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 import dj_database_url
-load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Cargar las variables del .env forzando la ruta exacta
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,10 +80,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 
